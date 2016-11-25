8a83e9b259bf625810c518f1161f1bcedd028cd2
ulises <ulises.lch1@gmail.com>
2016-11-25 06:59:18 -0500

    13

--- ioteca_web/ioteca_web_apps/inicio_web/controllers/categorias.js
+++ ioteca_web/ioteca_web_apps/inicio_web/controllers/categorias.js
@@ -11,6 +11,11 @@ app
 
     
     //$window.location = "#" + $location.path();
+     $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });
 
     $scope.list = function(params) {
         $scope.isLoading = true;
@@ -31,6 +36,7 @@ app
         params.query = $scope.query;
         $scope.list(params);
     };
+
 
     $scope.onReorder = function(order) { //TODO
         $log.log('Order: ' + order);
